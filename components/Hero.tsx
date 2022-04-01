e344c3c188310869f36dd5f85e9db86080797451
Trey Hollins <trey.hollins@gmail.com>
2022-04-01 19:38:10 -0400

    Navbar, Hero, and Footer updates

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Hero(): JSX.Element {
+    return (
+        <div className="flex w-full h-screen justify-center bg-[url('../images/hero4.avif')] bg-no-repeat bg-cover bg-fixed bg-center z-0 ">
+            <div className="justify-around content-around w-full pt-24">
+                <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">David Hollins</h1>
+                <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">Software Developer</h1>
+                <br />
+                <p className="font-medium text-lg sm:text-xl text-[#070723] text-center">
+                    I'm a graduate of the University of Cincinnati looking for a Software Developer position
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
