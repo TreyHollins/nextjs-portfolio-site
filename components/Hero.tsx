859911f1ab4bfdaca445e9abd1d0bd5223c00b91
Trey Hollins <trey.hollins@gmail.com>
2022-04-24 19:31:50 -0400

    Change hero image to improve load time

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero(): JSX.Element {
     return (
-        <div className="flex w-full h-screen justify-center bg-[url('../images/hero.jpg')] bg-no-repeat bg-cover bg-fixed bg-center z-0 ">
+        <div className="flex w-full h-screen justify-center bg-[url('../images/heroimage.jpg')] bg-no-repeat bg-cover bg-fixed bg-center z-0 ">
             <div className="justify-around content-around w-full pt-24">
                 <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">David Hollins</h1>
                 <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">Software Developer</h1>
