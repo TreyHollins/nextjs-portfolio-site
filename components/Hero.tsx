8f65deb1cf766d19d5abd50b019eee0340b3f6e0
Trey Hollins <trey.hollins@gmail.com>
2024-03-22 12:00:31 -0400

    Fix main text in center of screen

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero(): JSX.Element {
     return (
         <div className="flex w-full h-screen justify-center bg-[url('../images/heroimage.jpg')] bg-no-repeat bg-cover bg-fixed bg-center z-0 ">
-            <div className="justify-around content-around w-full pt-24">
+            <div className="justify-around w-full pt-24">
                 <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">David Hollins</h1>
                 <h1 className="text-4xl font font-bold tracking-tight text-[#070723] sm:text-7xl text-center">Software Developer</h1>
                 <br />
